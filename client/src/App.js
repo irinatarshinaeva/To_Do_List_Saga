f7e16350838eb3f5fd6059e38b887c78eae35220
Irina <tarschinaeva.irina@yandex.ru>
2021-01-14 11:39:12 +0300

    ready app and index

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Header from './Components/Header';
+import Redux from './Components/Redux';
+import Context from './Components/Context';
+
+import Saga from './Components/Saga';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <h1>Welcome</h1>
+          </Route>
+          <Route exact path="/redux">
+            <Redux />
+          </Route>
+          <Route exact path="/context">
+            <Context />
+          </Route>
+          <Route exact path="/saga">
+            <Saga />
+          </Route>
+
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
